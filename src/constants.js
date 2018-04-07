d4a86fec43de5cd02e54e1eee77f1dccae3f44b0
MibenCoop <mindkoop@gmail.com>
2018-04-07 13:27:28 +0300

    Cleaning index js and add constansts

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const TOKEN = '573448570:AAFYdmnoaPz9nkosrdHMaagIk1nJGpqAsTc';
+export const PASSWORD = "1234";
+export const START = 
+`Hello. I help you to get all statistics from your account
+Main commands:
+/help - display commands, which you can use
+/setAdmin {password} - get administrator rights(use space and password after this command)
+/getMessages - get amount of messages(require administator rights)
+`
+
+export const HELP =  
+`
+/setAdmin {password} - get administrator rights(use space and password after this command)         
+/getMessages - get amount of messages(require administator rights)
+`
--- src/index.js
+++ src/index.js
@@ -3,45 +3,39 @@ import request from 'request';
 import fetch from 'node-fetch';
 import axios from 'axios'
 import api from './api'
-const TOKEN = '573448570:AAFYdmnoaPz9nkosrdHMaagIk1nJGpqAsTc';
+import { TOKEN, PASSWORD, START, HELP } from './constants'
 const bot = new TelegramBot(TOKEN, { polling: true});
-const PASSWORD = "1234";
 
-
-/* bot.on('message', msg => {
-  if ( msg.text !== "/start" && msg.text !== "/getMessages" && msg.text !== "/setAdmin") {
+bot.on('message', msg => {
+  if ( !isCommand(msg.text) ) {
     const body = { msg };
     fetch('http://localhost:8080/api/bot', { 
-        method: 'POST',
-        body:    JSON.stringify(body),
-        headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body:    JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' },
     })
     .then(res => res.json())
     bot.sendMessage(msg.chat.id, "Your message has been saved");
   }
-}); */
+});
 
 bot.onText(/\/start/, function (msg, match) {
-    const fromId = msg.from.id;
-    const resp = "Hello. I am bot and can help you to get all statistics from your account"
+  const fromId = msg.from.id;
+  const resp = START;
     bot.sendMessage(fromId, resp);
+  }); 
+  
+  bot.onText(/\/help/, function (msg, match) {
+    const fromId = msg.from.id;
+  const resp = HELP;
+  bot.sendMessage(fromId, resp);
 }); 
-
-const getMessages = (admin, fromId) => {
-  if (admin === true ) {
-    api.messages.getMessages()
-      .then(res => bot.sendMessage(fromId, `Messages: ${res.length}`))
-      .catch(err => bot.semdMessage(fromId, err))
-  } else {
-    bot.sendMessage(fromId, "You don't have administrator rights")
-  }
-}
 
 bot.onText(/\/getMessages/, function (msg, match) {
   const fromId = msg.from.id;
   api.admin.getAdmin(msg)
-    .then(res => getMessages(res, fromId))
-    .catch(err => console.log('err',err))
+  .then(res => getMessages(res, fromId))
+  .catch(err => console.log('err',err))
 }); 
 
 bot.onText(/\/setAdmin/, function(msg, match) {
@@ -49,17 +43,35 @@ bot.onText(/\/setAdmin/, function(msg, match) {
   const password = (match.input).split(" ")[1];
   if ( password === PASSWORD) {
     api.admin.setAdmin(msg)
-      .then(res => bot.sendMessage(fromId, res))
-      .catch(err => bot.sendMessage(fromId, err))
+    .then(res => bot.sendMessage(fromId, res))
+    .catch(err => bot.sendMessage(fromId, err))
+  } 
+  else if (password === undefined ) {
+    bot.sendMessage(fromId, "Use space and password after this command")
   }
-  else { bot.sendMessage(fromId, "Invalid password") }
+  else { 
+    bot.sendMessage(fromId, "Invalid password") 
+  }
 })
 
+const getMessages = (admin, fromId) => {
+  if (admin === true ) {
+    api.messages.getMessages()
+    .then(res => bot.sendMessage(fromId, `Messages: ${res.length}`))
+    .catch(err => bot.semdMessage(fromId, err))
+  } else {
+    bot.sendMessage(fromId, "You don't have administrator rights")
+  }
+}
+
+const isCommand = (text) => 
+  (text.match(/\/(.+)/) === null) ? false : true;
+
+  
 
 
 
+  
 
 
-
-
-
+  
